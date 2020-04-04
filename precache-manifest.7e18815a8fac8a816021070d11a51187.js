60eec3d0b7988a98a4854539a449edce2ba84158
NHMToan <54366132+NHMToan@users.noreply.github.com>
2020-04-04 21:20:03 +0700

    Updates

---
+++ precache-manifest.7e18815a8fac8a816021070d11a51187.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2815b78973a82018d184db3a297a2e16",
+    "url": "/mypage/index.html"
+  },
+  {
+    "revision": "2763af00b158275a0a70",
+    "url": "/mypage/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "d50c65cf1a6e84a44f36",
+    "url": "/mypage/static/css/main.038766bf.chunk.css"
+  },
+  {
+    "revision": "2763af00b158275a0a70",
+    "url": "/mypage/static/js/2.c7e1f55f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mypage/static/js/2.c7e1f55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50c65cf1a6e84a44f36",
+    "url": "/mypage/static/js/main.2e99f41b.chunk.js"
+  },
+  {
+    "revision": "4e75f3ad61c0a13b0728",
+    "url": "/mypage/static/js/runtime-main.117f8d76.js"
+  },
+  {
+    "revision": "808e801791a7d80d0e38fbd8fb0e68d1",
+    "url": "/mypage/static/media/avatar.808e8017.jpg"
+  },
+  {
+    "revision": "b86afb4f32788fb3be358a7f050e7829",
+    "url": "/mypage/static/media/bg_pattern.b86afb4f.png"
+  },
+  {
+    "revision": "277706fedcf440dfa9fd535ef7285c82",
+    "url": "/mypage/static/media/fa-brands-400.277706fe.svg"
+  },
+  {
+    "revision": "586bdc7d039fe13bda883d81bc822a0e",
+    "url": "/mypage/static/media/fa-brands-400.586bdc7d.eot"
+  },
+  {
+    "revision": "68a68036d1804de9dd28565a4b860933",
+    "url": "/mypage/static/media/fa-brands-400.68a68036.woff2"
+  },
+  {
+    "revision": "6a004ddb259f02543b250c8467669ea6",
+    "url": "/mypage/static/media/fa-brands-400.6a004ddb.ttf"
+  },
+  {
+    "revision": "8270e503193150ebf94304681b327961",
+    "url": "/mypage/static/media/fa-brands-400.8270e503.woff"
+  },
+  {
+    "revision": "23fbd7c6763eca9c0d6079b3c56c5b1f",
+    "url": "/mypage/static/media/fa-light-300.23fbd7c6.woff2"
+  },
+  {
+    "revision": "5024e9b25bf8074067819efc65c4bd66",
+    "url": "/mypage/static/media/fa-light-300.5024e9b2.woff"
+  },
+  {
+    "revision": "7fc88777fc3ee282f4bda2ff4a6fff9a",
+    "url": "/mypage/static/media/fa-light-300.7fc88777.svg"
+  },
+  {
+    "revision": "80329a35cee0268dc6cf640911ad9bab",
+    "url": "/mypage/static/media/fa-light-300.80329a35.ttf"
+  },
+  {
+    "revision": "fd4e45701b8f0fcdb5b958e191075437",
+    "url": "/mypage/static/media/fa-light-300.fd4e4570.eot"
+  },
+  {
+    "revision": "2fa6bcb87bcf488cbc93e7391cb3373a",
+    "url": "/mypage/static/media/fa-regular-400.2fa6bcb8.woff2"
+  },
+  {
+    "revision": "559f4a5f8ad7c7b28626743cadc72a27",
+    "url": "/mypage/static/media/fa-regular-400.559f4a5f.woff"
+  },
+  {
+    "revision": "b2d220369f80715b91aa3f75447f5895",
+    "url": "/mypage/static/media/fa-regular-400.b2d22036.eot"
+  },
+  {
+    "revision": "bdd14bcd205c27612b5bfe322ea6598a",
+    "url": "/mypage/static/media/fa-regular-400.bdd14bcd.ttf"
+  },
+  {
+    "revision": "dc1995da1b48ebf442e874cc63d9c989",
+    "url": "/mypage/static/media/fa-regular-400.dc1995da.svg"
+  },
+  {
+    "revision": "3a6ff68146d68033d0c4bd44783ee343",
+    "url": "/mypage/static/media/fa-solid-900.3a6ff681.ttf"
+  },
+  {
+    "revision": "4279e7972a1ec6d434b560eb06e1f83d",
+    "url": "/mypage/static/media/fa-solid-900.4279e797.eot"
+  },
+  {
+    "revision": "5e46e9273987b5d795c439f84526f147",
+    "url": "/mypage/static/media/fa-solid-900.5e46e927.svg"
+  },
+  {
+    "revision": "893b3ef2bc8b4d8979798fb3b28132bd",
+    "url": "/mypage/static/media/fa-solid-900.893b3ef2.woff2"
+  },
+  {
+    "revision": "a2a8932315d40d4fa55cf32162177ca3",
+    "url": "/mypage/static/media/fa-solid-900.a2a89323.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/mypage/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/mypage/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/mypage/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/mypage/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/mypage/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
